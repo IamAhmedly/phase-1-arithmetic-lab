dc19430920414745d80a5f8741e8638969435cb6
Ahmedly SAM <ahmed.esam@duc.edu.iq>
2024-02-21 23:59:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 40;
+let num4 = 6;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  1,
+  2,
+  3,
+  4,
+  5
+);
